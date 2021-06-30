d960e1c68cbe44b4a1ef8b44f707a8aa29f89ae8
Felipe Leandro Aguazaco <leandro.aguazaco@outlook.com>
2021-06-29 23:33:38 -0500

    Sampling Models
    A sampling model models the random behavior of a process as the sampling of draws from an urn.

--- 03-Random_Variables_Sampling_Models_CLT.R
+++ 03-Random_Variables_Sampling_Models_CLT.R
@@ -5,3 +5,65 @@
 beads <- rep(c("red", "blue"), times = c(2, 3))
 X <- ifelse(sample(beads, 1) == "blue", 1, 0)
 X
+
+# Sampling Models ====
+
+# A sampling model models the random behavior of a process as the sampling of draws from an urn.
+
+color <- rep(x = c("Red", "Black", "Green"), 
+             times = c(18, 18, 2))
+color
+
+X <- sample(x = ifelse(color == "Red", -1, 1), 
+            size = 1000, 
+            replace = TRUE)
+X[1:10]
+
+X <- sample(x = c(-1, 1), # Sampling model
+            size = 1000, 
+            replace = TRUE,
+            prob = c(9/19, 10/19))
+S <- sum(X)
+S
+
+# Probability distribution of a random variable: probability of the observed
+# value falling in any given interval
+
+n <- 1000
+B <- 10^4
+S <- replicate(n = B, expr = {
+  X <- sample(c(-1, 1), 
+              size = n, 
+              replace = TRUE, 
+              prob = c(9/19, 10/19))
+  S <- sum(X)
+})
+
+s <- seq(min(S), max(S), length = 100)
+normal_density <- data.frame(s, f = dnorm(s, mean(S), sd(S)))
+
+data.frame(S) %>% 
+  ggplot(aes(x = S, y = ..density..)) +
+    geom_histogram(color = "black", 
+                   binwidth = 8) +
+    labs(y = "Probability") +
+    geom_line(data = normal_density, 
+              mapping = aes(x = s,
+                            y = f), 
+              color = "blue") + 
+    theme_bw()
+
+mean(S < 0)
+
+sample_quantiles <- quantile(S, p)
+theoretical_quantiles <- qnorm(p, mean = mean(S), sd = sd(S))
+
+ggplot(data = NULL,
+       mapping = aes(x = theoretical_quantiles, 
+                     y = sample_quantiles)) + 
+  geom_point() +
+  geom_abline() + 
+  theme_bw()
+
+# The average of many draws of a random variable is called its expected value.
+# The standard deviation of many draws of a random variable is called its standard error.
